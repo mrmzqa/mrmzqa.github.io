bae596476b14c5a7bbae9649ec39b57b573d2b32
mrmzqa <mrmzqa@gmail.com>
2025-08-25 23:14:48 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  loadEnv(mode, process.cwd(), ""); // Optional if not using any env vars
+
   return {
-    base: `/${env.VITE_REPO_NAME}/`,
+    base: '/', // User/Org page - no subdirectory
     plugins: [react(), tailwindcss()],
     server: {
       open: true,
-      host: false, // Allow access from network devices
+      host: false,
       proxy: {
         "/api": {
           target: "http://localhost:5000",
