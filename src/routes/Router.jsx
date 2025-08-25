a2f6a02c971c8b66a84ce34b0da3173d3e143837
mrmzqa <mrmzqa@gmail.com>
2025-08-25 23:23:33 +0300

    canges

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,12 @@
 import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Loading from "../components/common/loading/Loading";
+
+// Lazy-loaded components
 const Home = lazy(() => import("../pages/Home"));
 const Main = lazy(() => import("../layouts/Main"));
 
-const repoName = import.meta.env.VITE_REPO_NAME || "";
-
+// No need for repoName or basename if it's a user site
 export const router = createBrowserRouter(
   [
     {
@@ -18,10 +19,9 @@ export const router = createBrowserRouter(
       children: [
         {
           path: "/",
-          element: <Home></Home>,
+          element: <Home />,
         },
       ],
     },
-  ],
-  { basename: `/${repoName}` }
+  ]
 );
